a31e01408ef6a35ea26eb148ec979c2b561c86ea
martin_vasconcelo <Marto Pc@MartoVasc.local>
2019-11-18 18:50:48 -0300

    Proyecto API TMovieDatabase, faltan Features

---
+++ controllers/ctrl.js
@@ -0,0 +1,215 @@
+const axios = require('axios');
+const queryString = require('query-string');
+const API_KEY = "658d34148bcfeeac411fef3a8e6cea76";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const ctrl = {};
+
+ctrl.index = async function (req, res, next) {
+   
+    //Este Objeto se llena con los objetos que queremos mostrar.
+    let filters = {
+        api_key: API_KEY,
+        language: 'en-EN'
+    };
+    //Acá le agrego un género a la busqueda.
+    //filters.with_genres = '28';
+
+    filters.page = 9;
+
+    let query = queryString.stringify(filters);
+
+    await axios.get(`${BASE_URL}/discover/movie/?${query}`)
+        .then(({
+            data
+        }) => {
+            console.log(data.results)
+            //Acá esta el render que mostramos en el index.ejs
+            res.render('index', {
+                movies: data.results,
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+        
+};
+
+
+ctrl.pagina1 = async function (req, res, next) {
+   
+    //Este Objeto se llena con los objetos que queremos mostrar.
+    let filters = {
+        api_key: API_KEY,
+        language: 'es-ES'
+    };
+    //Acá le agrego un género a la busqueda.
+    //filters.with_genres = '28';
+
+    filters.page = 45;
+
+    let query = queryString.stringify(filters);
+
+    await axios.get(`${BASE_URL}/discover/movie/?${query}`)
+        .then(({
+            data
+        }) => {
+            console.log(data.results)
+            //Acá esta el render que mostramos en el index.ejs
+            res.render('index', {
+                movies: data.results,
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+        
+};
+
+ctrl.pagina2 = async function (req, res, next) {
+   
+    //Este Objeto se llena con los objetos que queremos mostrar.
+    let filters = {
+        api_key: API_KEY,
+        language: 'pt-BR'
+    };
+    //Acá le agrego un género a la busqueda.
+    //filters.with_genres = '28';
+
+    filters.page = 24;
+
+    let query = queryString.stringify(filters);
+
+    await axios.get(`${BASE_URL}/discover/movie/?${query}`)
+        .then(({
+            data
+        }) => {
+            console.log(data.results)
+            //Acá esta el render que mostramos en el index.ejs
+            res.render('index', {
+                movies: data.results,
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+        
+};
+
+
+ctrl.pagina3 = async function (req, res, next) {
+   
+    //Este Objeto se llena con los objetos que queremos mostrar.
+    let filters = {
+        api_key: API_KEY,
+        language: 'cn-CN'
+    };
+    //Acá le agrego un género a la busqueda.
+    //filters.with_genres = '28';
+
+    filters.page = 17;
+
+    let query = queryString.stringify(filters);
+
+    await axios.get(`${BASE_URL}/discover/movie/?${query}`)
+        .then(({
+            data
+        }) => {
+            console.log(data.results)
+            //Acá esta el render que mostramos en el index.ejs
+            res.render('index', {
+                movies: data.results,
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+        
+};
+
+
+ctrl.accion = async function (req, res, next) {
+   
+    //Este Objeto se llena con los objetos que queremos mostrar.
+    let filters = {
+        api_key: API_KEY,
+        language: 'es-ES'
+    };
+    //Acá le agrego un género a la busqueda.
+    filters.with_genres = '28';
+
+    let query = queryString.stringify(filters);
+
+    await axios.get(`${BASE_URL}/discover/movie/?${query}`)
+        .then(({
+            data
+        }) => {
+            console.log(data.results)
+            //Acá esta el render que mostramos en el index.ejs
+            res.render('index', {
+                movies: data.results,
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+        
+};
+
+
+ctrl.infantil = async function (req, res, next) {
+   
+    //Este Objeto se llena con los objetos que queremos mostrar.
+    let filters = {
+        api_key: API_KEY,
+        language: 'es-ES'
+    };
+    //Acá le agrego un género a la busqueda.
+    //filters.with_genres = '28';
+
+    filters.with_genres = '16';
+
+    let query = queryString.stringify(filters);
+
+    await axios.get(`${BASE_URL}/discover/movie/?${query}`)
+        .then(({
+            data
+        }) => {
+            console.log(data.results)
+            //Acá esta el render que mostramos en el index.ejs
+            res.render('index', {
+                movies: data.results,
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+        
+};
+
+
+ctrl.terror = async function (req, res, next) {
+   
+    //Este Objeto se llena con los objetos que queremos mostrar.
+    let filters = {
+        api_key: API_KEY,
+        language: 'es-ES'
+    };
+    //Acá le agrego un género a la busqueda.
+    //filters.with_genres = '28';
+
+    filters.with_genres = '27';
+
+    let query = queryString.stringify(filters);
+
+    await axios.get(`${BASE_URL}/discover/movie/?${query}`)
+        .then(({
+            data
+        }) => {
+            console.log(data.genre_ids)
+            //Acá esta el render que mostramos en el index.ejs
+            res.render('index', {
+                movies: data.results,
+
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+        
+};
+
+module.exports = ctrl;
